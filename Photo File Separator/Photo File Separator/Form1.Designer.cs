ed8af372ab00a3b0ea06d507f7c4d8de716beac6
litao <lt.dygzs@qq.com>
2020-06-26 16:42:44 +0800

    修改ui

--- Photo File Separator/Photo File Separator/Form1.Designer.cs
+++ Photo File Separator/Photo File Separator/Form1.Designer.cs
@@ -61,9 +61,10 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(16, 233);
+            this.button1.Location = new System.Drawing.Point(18, 280);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(555, 91);
+            this.button1.Size = new System.Drawing.Size(624, 109);
             this.button1.TabIndex = 9;
             this.button1.Text = "拖动文件到这里或点击选择文件";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,9 +74,10 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(430, 48);
+            this.button2.Location = new System.Drawing.Point(484, 58);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(141, 35);
+            this.button2.Size = new System.Drawing.Size(159, 42);
             this.button2.TabIndex = 2;
             this.button2.Text = "选择输出文件夹";
             this.button2.UseVisualStyleBackColor = true;
@@ -83,62 +85,64 @@
             // 
             // textBox2
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(101, 84);
+            this.textBox2.Location = new System.Drawing.Point(114, 101);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(141, 25);
+            this.textBox2.Size = new System.Drawing.Size(158, 28);
             this.textBox2.TabIndex = 3;
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 87);
+            this.label1.Location = new System.Drawing.Point(18, 104);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.Size = new System.Drawing.Size(80, 18);
             this.label1.TabIndex = 4;
             this.label1.Text = "去掉后缀";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 56);
+            this.label2.Location = new System.Drawing.Point(18, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.Size = new System.Drawing.Size(80, 18);
             this.label2.TabIndex = 6;
             this.label2.Text = "去掉前缀";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(101, 53);
+            this.textBox3.Location = new System.Drawing.Point(114, 64);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(141, 25);
+            this.textBox3.Size = new System.Drawing.Size(158, 28);
             this.textBox3.TabIndex = 1;
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(345, 152);
+            this.label3.Location = new System.Drawing.Point(388, 182);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.Size = new System.Drawing.Size(80, 18);
             this.label3.TabIndex = 8;
             this.label3.Text = "统一命名";
             // 
             // textBox4
             // 
-            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox4.Location = new System.Drawing.Point(430, 149);
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(484, 179);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(141, 25);
+            this.textBox4.Size = new System.Drawing.Size(158, 28);
             this.textBox4.TabIndex = 7;
             // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(167, 53);
+            this.button3.Location = new System.Drawing.Point(188, 64);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 25);
+            this.button3.Size = new System.Drawing.Size(84, 30);
             this.button3.TabIndex = 9;
             this.button3.Text = "彩蛋";
             this.button3.UseVisualStyleBackColor = true;
@@ -146,45 +150,51 @@
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(345, 90);
+            this.label4.Location = new System.Drawing.Point(388, 108);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.Size = new System.Drawing.Size(80, 18);
             this.label4.TabIndex = 13;
             this.label4.Text = "增加前缀";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(430, 87);
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(484, 104);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(141, 25);
+            this.textBox5.Size = new System.Drawing.Size(158, 28);
             this.textBox5.TabIndex = 4;
             // 
             // label5
             // 
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(345, 121);
+            this.label5.Location = new System.Drawing.Point(388, 145);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.Size = new System.Drawing.Size(80, 18);
             this.label5.TabIndex = 11;
             this.label5.Text = "增加后缀";
             // 
             // textBox6
             // 
             this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox6.Location = new System.Drawing.Point(430, 118);
+            this.textBox6.Location = new System.Drawing.Point(484, 142);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(141, 25);
+            this.textBox6.Size = new System.Drawing.Size(158, 28);
             this.textBox6.TabIndex = 6;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.rbDrawable);
             this.groupBox1.Controls.Add(this.rbMipmap);
-            this.groupBox1.Location = new System.Drawing.Point(13, 115);
+            this.groupBox1.Location = new System.Drawing.Point(15, 138);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(229, 53);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(258, 64);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "图片类型";
@@ -192,9 +202,10 @@
             // rbDrawable
             // 
             this.rbDrawable.AutoSize = true;
-            this.rbDrawable.Location = new System.Drawing.Point(114, 22);
+            this.rbDrawable.Location = new System.Drawing.Point(128, 26);
+            this.rbDrawable.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.rbDrawable.Name = "rbDrawable";
-            this.rbDrawable.Size = new System.Drawing.Size(92, 19);
+            this.rbDrawable.Size = new System.Drawing.Size(105, 22);
             this.rbDrawable.TabIndex = 1;
             this.rbDrawable.Text = "drawable";
             this.rbDrawable.UseVisualStyleBackColor = true;
@@ -203,9 +214,10 @@
             // 
             this.rbMipmap.AutoSize = true;
             this.rbMipmap.Checked = true;
-            this.rbMipmap.Location = new System.Drawing.Point(32, 22);
+            this.rbMipmap.Location = new System.Drawing.Point(36, 26);
+            this.rbMipmap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.rbMipmap.Name = "rbMipmap";
-            this.rbMipmap.Size = new System.Drawing.Size(76, 19);
+            this.rbMipmap.Size = new System.Drawing.Size(87, 22);
             this.rbMipmap.TabIndex = 0;
             this.rbMipmap.TabStop = true;
             this.rbMipmap.Text = "mipmap";
@@ -217,9 +229,11 @@
             this.groupBox2.Controls.Add(this.radioButton3);
             this.groupBox2.Controls.Add(this.radioButton1);
             this.groupBox2.Controls.Add(this.radioButton2);
-            this.groupBox2.Location = new System.Drawing.Point(13, 174);
+            this.groupBox2.Location = new System.Drawing.Point(15, 209);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(558, 53);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(628, 64);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "重复策略";
@@ -227,9 +241,10 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(336, 22);
+            this.radioButton4.Location = new System.Drawing.Point(378, 26);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(58, 19);
+            this.radioButton4.Size = new System.Drawing.Size(69, 22);
             this.radioButton4.TabIndex = 3;
             this.radioButton4.Text = "覆盖";
             this.radioButton4.UseVisualStyleBackColor = true;
@@ -238,9 +253,10 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(270, 22);
+            this.radioButton3.Location = new System.Drawing.Point(304, 26);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(58, 19);
+            this.radioButton3.Size = new System.Drawing.Size(69, 22);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.Text = "忽略";
             this.radioButton3.UseVisualStyleBackColor = true;
@@ -249,9 +265,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(114, 22);
+            this.radioButton1.Location = new System.Drawing.Point(128, 26);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(148, 19);
+            this.radioButton1.Size = new System.Drawing.Size(177, 22);
             this.radioButton1.TabIndex = 1;
             this.radioButton1.Text = "复制到新文件夹中";
             this.radioButton1.UseVisualStyleBackColor = true;
@@ -260,9 +277,10 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Checked = true;
-            this.radioButton2.Location = new System.Drawing.Point(32, 22);
+            this.radioButton2.Location = new System.Drawing.Point(36, 26);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(74, 19);
+            this.radioButton2.Size = new System.Drawing.Size(87, 22);
             this.radioButton2.TabIndex = 0;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "尾数+n";
@@ -270,18 +288,21 @@
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.FormattingEnabled = true;
-            this.textBox1.Location = new System.Drawing.Point(13, 19);
+            this.textBox1.Location = new System.Drawing.Point(15, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.MaxDropDownItems = 10;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(558, 23);
+            this.textBox1.Size = new System.Drawing.Size(627, 26);
             this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 336);
+            this.ClientSize = new System.Drawing.Size(656, 403);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -298,6 +319,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
