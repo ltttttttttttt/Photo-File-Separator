b11ae7a3dadbaac441645eb3cfbfc3c8c923e713
lt <1764780379@qq.com>
2021-04-12 19:33:00 +0800

    Update main.kt

--- Compose/src/main/kotlin/main.kt
+++ Compose/src/main/kotlin/main.kt
@@ -1,20 +1,46 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 
-fun main() = Window {
+fun main() = Window(
+    title = "UI图分离器",
+) {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        topInput()
+    }
+}
+
+@Composable
+fun topInput() {
+    var etText by remember { mutableStateOf("") }
+    Row {
+        TextField(
+            value = etText,
+            onValueChange = {
+                etText = it
+            },
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            modifier = Modifier.height(45.dp).weight(1f).padding(0.dp)
+        )
+        Button(
+            {
+
+            },
+            modifier = Modifier.height(45.dp)
+        ) {
+            Text("选择输出文件夹")
         }
     }
 }
