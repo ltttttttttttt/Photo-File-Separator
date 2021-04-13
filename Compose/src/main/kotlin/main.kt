60df8041051cb07197ff2d4c81bef27a54a92bd6
lt <1764780379@qq.com>
2021-04-13 19:35:10 +0800

    新增部分代码(学习ing)

--- Compose/src/main/kotlin/main.kt
+++ Compose/src/main/kotlin/main.kt
@@ -1,25 +1,59 @@
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import view.settingInput
+
+//仿微信: http://www.qishunwang.net/news_show_67329.aspx
 
 fun main() = Window(
     title = "UI图分离器",
 ) {
-    var text by remember { mutableStateOf("Hello, World!") }
+    MaterialTheme() {
+        Column(Modifier.padding(8.dp)) {
+            topInput()
+            Row {
 
-    MaterialTheme {
-        topInput()
+                settings()
+            }
+        }
     }
 }
 
+@Composable
+fun settings() {
+    Column {
+        settingInput("去掉前缀") {
+
+        }
+        Spacer(Modifier.height(4.dp))
+        settingInput("去掉后缀") {
+
+        }
+        Spacer(Modifier.height(4.dp))
+        settingInput("增加前缀") {
+
+        }
+        Spacer(Modifier.height(4.dp))
+        settingInput("增加后缀") {
+
+        }
+        Spacer(Modifier.height(4.dp))
+        settingInput("统一命名") {
+
+        }
+    }
+}
+
+//顶部地址栏
 @Composable
 fun topInput() {
     var etText by remember { mutableStateOf("") }
@@ -29,16 +63,17 @@ fun topInput() {
             onValueChange = {
                 etText = it
             },
-            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+            shape = RoundedCornerShape(0.dp),
             maxLines = 1,
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-            modifier = Modifier.height(45.dp).weight(1f).padding(0.dp)
+            modifier = Modifier.height(55.dp).weight(1f)
         )
+        Spacer(Modifier.width(16.dp))
         Button(
             {
 
             },
-            modifier = Modifier.height(45.dp)
+            modifier = Modifier.height(55.dp)
         ) {
             Text("选择输出文件夹")
         }
---
+++ Compose/src/main/kotlin/view/SettingInput.kt
@@ -0,0 +1,47 @@
+package view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+
+/**
+ * creator: lt  2021/4/13  lt.dygzs@qq.com
+ * effect : 设置的输入view
+ * warning:
+ */
+@Composable
+fun settingInput(
+    leftText: String,
+    onTextChangeListener: (String) -> Unit
+) {
+    var etText by remember { mutableStateOf("") }
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(leftText)
+        Spacer(Modifier.width(8.dp))
+        TextField(
+            value = etText,
+            onValueChange = {
+                etText = it
+                onTextChangeListener(it)
+            },
+//            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+            shape = RoundedCornerShape(0.dp),
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            modifier = Modifier.height(60.dp).width(150.dp).padding(2.dp),
+            textStyle = MaterialTheme.typography.body1,
+        )
+    }
+}
